fc71900bb87a5ad3af94e479cda8dc943328893a
Jeremy J Starcher <jeremy.starcher@gmail.com>
2026-06-25 17:24:25 -0400

    main - Changed text to labels.

--- go
+++ go
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm yahtzee.65o
+rm scores/strings.m65
+
 ( cd scores/; ./scores.py )
 #../code/atasm -xd2.atr -gfile.lst  ./yahtzee.m65 && ../code/atari800 yahtzee.65o
 ../code/atasm -xd2.atr ./yahtzee.m65 && ../code/atari800 yahtzee.65o
--- scores/scores.py
+++ scores/scores.py
@@ -26,25 +26,92 @@ class Chrome:
     row: int
     txt: str
 
-def get_sheet() -> list[Chrome]:
+@dataclass
+class ScreenLabel:
+    key: str
+    value: str
+
+def get_sheet_old() -> list[Chrome]:
     sheet = [
         Chrome('L00', 0,  0, " ┌────────────┤ FIVE DICE ├───────────┐ "),
-        Chrome('L01', 0,  1, " |ACES       #1C##  |3 OF KIND  #3K## | "),
-        Chrome('L02', 0,  2, " |TWOS       #2C##  |4 OF KIND  #4K## | "),
-        Chrome('L03', 0,  3, " |THREES     #3C##  |FULL HOUSE #FH## | "),
-        Chrome('L04', 0,  4, " |FOURS      #4C##  |S STRAIGHT #SS## | "),
-        Chrome('L05', 0,  5, " |FIVES      #5C##  |L STRAIGHT #LS## | "),
-        Chrome('L06', 0,  6, " |SIXES      #6C##  |5 OF KIND  #5K## | "),
-        Chrome('L07', 0,  7, " |TOP SCORE  #TS##  |CHANCE     #CH## | "),
-        Chrome('L08', 0,  8, " |BONUS      #TB##  |5K BONUS   #5B## | "),
-        Chrome('L09', 0,  9, " |TOTAL      #UT##  |TOTAL      #LT## | "),
+        Chrome('L01', 0,  1, " |ACES       #S1C#  |3 OF KIND  #S3K# | "),
+        Chrome('L02', 0,  2, " |TWOS       #S2C#  |4 OF KIND  #S4K# | "),
+        Chrome('L03', 0,  3, " |THREES     #S3C#  |FULL HOUSE #SFH# | "),
+        Chrome('L04', 0,  4, " |FOURS      #S4C#  |S STRAIGHT #SSS# | "),
+        Chrome('L05', 0,  5, " |FIVES      #S5C#  |L STRAIGHT #SLS# | "),
+        Chrome('L06', 0,  6, " |SIXES      #S6C#  |5 OF KIND  #S5K# | "),
+        Chrome('L07', 0,  7, " |TOP SCORE  #STS#  |CHANCE     #SCH# | "),
+        Chrome('L08', 0,  8, " |BONUS      #STB#  |5K BONUS   #S5B# | "),
+        Chrome('L09', 0,  9, " |TOTAL      #SUT#  |TOTAL      #SLT# | "),
         Chrome('L09', 0,  9, " ├───────┬──────────┴─────────┬───────┤ "),
         Chrome('L10', 0, 10, " | ♥♣♦♠  | Grand Total  #GT## | ␛↑␛↓␛←␛→  | "),
         Chrome('L09', 0,  9, " └───────┴────────────────────┴───────┘ "),
- 
- 
     ]
     return sheet
+
+def get_sheet() -> list[Chrome]:
+    sheet = [
+        Chrome('L00', 0,  0, " ┌────────────┤ FIVE DICE ├───────────┐ "),
+        Chrome('L01', 0,  1, " |#L1C######  #S1C# |#L3K###### #S3K# | "),
+        Chrome('L02', 0,  2, " |#L2C######  #S2C# |#L4K###### #S4K# | "),
+        Chrome('L03', 0,  3, " |#L3C######  #S3C# |#LFH###### #SFH# | "),
+        Chrome('L04', 0,  4, " |#L4C######  #S4C# |#LSS###### #SSS# | "),
+        Chrome('L05', 0,  5, " |#L5C######  #S5C# |#LLS###### #SLS# | "),
+        Chrome('L06', 0,  6, " |#L6C######  #S6C# |#L5K###### #S5K# | "),
+        Chrome('L07', 0,  7, " |#LTS######  #STS# |#LCH###### #SCH# | "),
+        Chrome('L08', 0,  8, " |#LTB######  #STB# |#L5B###### #S5B# | "),
+        Chrome('L09', 0,  9, " |#LUT######  #SUT# |#LLT###### #SLT# | "),
+        Chrome('L09', 0,  9, " ├───────┬──────────┴─────────┬───────┤ "),
+        Chrome('L10', 0, 10, " | ♥♣♦♠  | #GTT#######  #SGT# | ␛↑␛↓␛←␛→  | "),
+        Chrome('L09', 0,  9, " └───────┴────────────────────┴───────┘ "),
+    ]
+    return sheet
+
+def get_labels() -> list[ScreenLabel]:
+    num = '00000'
+    out: list[ScreenLabel] = [
+        ScreenLabel('L1C', 'Aces'), 
+        ScreenLabel('S1C', num),
+        ScreenLabel('L2C', 'Twos'),
+        ScreenLabel('S2C', num),
+        ScreenLabel('L3C', 'Threes'),
+        ScreenLabel('S3C', num),
+        ScreenLabel('L4C', 'Fours'),
+        ScreenLabel('S4C', num),
+        ScreenLabel('L5C', 'Fives'),
+        ScreenLabel('S5C', num),
+        ScreenLabel('L6C', 'Sixes'),
+        ScreenLabel('S6C', num),
+        ScreenLabel('LTS', 'Top Score'),
+        ScreenLabel('STS', num),
+        ScreenLabel('LTB', 'Upper Bonus'),
+        ScreenLabel('STB', num),
+        ScreenLabel('LUT', 'Upper Total'),
+        ScreenLabel('SUT', num),
+ 
+        ScreenLabel('L3K', '3 of a Kind'),
+        ScreenLabel('S3K', num),
+        ScreenLabel('L4K', '4 of a Kind'),
+        ScreenLabel('S4K', num),
+        ScreenLabel('LFH', 'Full House'),
+        ScreenLabel('SFH', num),
+        ScreenLabel('LSS', 'Small Straight'),
+        ScreenLabel('SSS', num),
+        ScreenLabel('LLS', 'Large Straight'),
+        ScreenLabel('SLS', num),
+        ScreenLabel('L5K', '5 of a Kind'),
+        ScreenLabel('S5K', num),
+        ScreenLabel('LCH', 'Chance'),
+        ScreenLabel('SCH', num),
+        ScreenLabel('L5B', '5 Kind Bonus'),
+        ScreenLabel('S5B', num),
+        ScreenLabel('LLT', 'Lower Total'),
+        ScreenLabel('SLT', num),
+
+        ScreenLabel('GTT', 'Grand Total'),
+        ScreenLabel('SGT', num),
+    ]
+    return out;
 
 def get_sheet_for_screen() -> list[Chrome]:
     sheet = get_sheet();
@@ -74,7 +141,7 @@ def get_sheet_for_screen() -> list[Chrome]:
             try:
                 idx = ATASCII.index(char)
             except ValueError:
-                print("Warning: character '{char}' is not ATASCII")
+                print(f"Warning: character '{char}' is not ATASCII")
 
 
             hex = '$' + f"{idx:02X}"
@@ -96,6 +163,8 @@ def convert_sheet_do_m65(sheet: list[Chrome]) -> list[str]:
 
 sheet = get_sheet_for_screen()
 sheet2 = convert_sheet_do_m65(sheet)
+
+labels = get_labels()
 
 with open(ASM_FILE_NAME, 'w') as file:
     file.write('MESSAGE\n')
