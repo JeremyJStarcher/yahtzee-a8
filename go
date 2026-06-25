eebb65582e85b2c082233640190ce32c33eab119
Jeremy J Starcher <jeremy.starcher@gmail.com>
2026-06-25 16:32:42 -0400

    main - Auto-includes the generated strings.

--- go
+++ go
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+( cd scores/; ./scores.py )
+#../code/atasm -xd2.atr -gfile.lst  ./yahtzee.m65 && ../code/atari800 yahtzee.65o
 ../code/atasm -xd2.atr ./yahtzee.m65 && ../code/atari800 yahtzee.65o
-#./atasm ./printsc.m65
 
--- scores/scores.py
+++ scores/scores.py
@@ -26,22 +26,23 @@ class Chrome:
     row: int
     txt: str
 
-
 def get_sheet() -> list[Chrome]:
     sheet = [
-        Chrome('L00', 0,  0, "┌────────────┤ FIVE DICE ├───────────┐"),
-        Chrome('L01', 0,  1, "|ACES       #1C##  |3 OF KIND  #3K## |"),
-        Chrome('L02', 0,  2, "|TWOS       #2C##  |4 OF KIND  #4K## |"),
-        Chrome('L03', 0,  3, "|THREES     #3C##  |FULL HOUSE #FH## |"),
-        Chrome('L04', 0,  4, "|FOURS      #4C##  |S STRAIGHT #SS## |"),
-        Chrome('L05', 0,  5, "|FIVES      #5C##  |L STRAIGHT #LS## |"),
-        Chrome('L06', 0,  6, "|SIXES      #6C##  |5 OF KIND  #5K## |"),
-        Chrome('L07', 0,  7, "|TOP SCORE  #TS##  |CHANCE     #CH## |"),
-        Chrome('L08', 0,  8, "|BONUS      #TB##  |5K BONUS   #5B## |"),
-        Chrome('L09', 0,  9, "|TOTAL      #UT##  |TOTAL      #LT## |"),
-        Chrome('L09', 0,  9, "├──────────────────┴─────────────────┤"),
-        Chrome('L10', 0, 10, "|         GRAND TOTAL  #GT##         |"),
-        Chrome('L09', 0,  9, "└────────────────────────────────────┘"),
+        Chrome('L00', 0,  0, " ┌────────────┤ FIVE DICE ├───────────┐ "),
+        Chrome('L01', 0,  1, " |ACES       #1C##  |3 OF KIND  #3K## | "),
+        Chrome('L02', 0,  2, " |TWOS       #2C##  |4 OF KIND  #4K## | "),
+        Chrome('L03', 0,  3, " |THREES     #3C##  |FULL HOUSE #FH## | "),
+        Chrome('L04', 0,  4, " |FOURS      #4C##  |S STRAIGHT #SS## | "),
+        Chrome('L05', 0,  5, " |FIVES      #5C##  |L STRAIGHT #LS## | "),
+        Chrome('L06', 0,  6, " |SIXES      #6C##  |5 OF KIND  #5K## | "),
+        Chrome('L07', 0,  7, " |TOP SCORE  #TS##  |CHANCE     #CH## | "),
+        Chrome('L08', 0,  8, " |BONUS      #TB##  |5K BONUS   #5B## | "),
+        Chrome('L09', 0,  9, " |TOTAL      #UT##  |TOTAL      #LT## | "),
+        Chrome('L09', 0,  9, " ├───────┬──────────┴─────────┬───────┤ "),
+        Chrome('L10', 0, 10, " | ♥♣♦♠  | Grand Total  #GT## | ␛↑␛↓␛←␛→  | "),
+        Chrome('L09', 0,  9, " └───────┴────────────────────┴───────┘ "),
+ 
+ 
     ]
     return sheet
 
@@ -93,12 +94,15 @@ def convert_sheet_do_m65(sheet: list[Chrome]) -> list[str]:
 
     return out;
 
-
 sheet = get_sheet_for_screen()
 sheet2 = convert_sheet_do_m65(sheet)
 
 with open(ASM_FILE_NAME, 'w') as file:
+    file.write('MESSAGE\n')
     file.write('\n'.join(sheet2))
+    file.write('\n')
+    file.write('MESSAGE_END\n')
+    file.write('MESSAGE_LEN = MESSAGE_END - MESSAGE\n\n')
 
-print(sheet2)
+# print(sheet2)
 
