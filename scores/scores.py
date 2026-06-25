d5b4a250c38c334b2994aa08b434964c7862f694
Jeremy J Starcher <jeremy.starcher@gmail.com>
2026-06-25 12:58:33 -0400

    Cleaner play sheet.

--- scores/scores.py
+++ scores/scores.py
@@ -29,17 +29,19 @@ class Chrome:
 
 def get_sheet() -> list[Chrome]:
     sheet = [
-        Chrome('L00', 0,  0, "-------------| FIVE DICE |------------"),
-        Chrome('L01', 0,  1, "ACES       #1C##   | 3 OF KIND  #3K## "),
-        Chrome('L02', 0,  2, "TWOS       #2C##   | 4 OF KIND  #4K## "),
-        Chrome('L03', 0,  3, "THREES     #3C##   | FULL HOUSE #FH## "),
-        Chrome('L04', 0,  4, "FOURS      #4C##   | S STRAIGHT #SS## "),
-        Chrome('L05', 0,  5, "FIVES      #5C##   | L STRAIGHT #LS## "),
-        Chrome('L06', 0,  6, "SIXES      #6C##   | 5 OF KIND  #5K## "),
-        Chrome('L07', 0,  7, "TOP SCORE  #TS##   | CHANCE     #CH## "),
-        Chrome('L08', 0,  8, "BONUS      #TB##   | 5K BONUS   #5B## "),
-        Chrome('L09', 0,  9, "TOTAL      #UT##   | TOTAL      #LT## "),
-        Chrome('L10', 0, 10, "          GRAND TOTAL  #GT##          "),
+        Chrome('L00', 0,  0, "┌────────────┤ FIVE DICE ├───────────┐"),
+        Chrome('L01', 0,  1, "|ACES       #1C##  |3 OF KIND  #3K## |"),
+        Chrome('L02', 0,  2, "|TWOS       #2C##  |4 OF KIND  #4K## |"),
+        Chrome('L03', 0,  3, "|THREES     #3C##  |FULL HOUSE #FH## |"),
+        Chrome('L04', 0,  4, "|FOURS      #4C##  |S STRAIGHT #SS## |"),
+        Chrome('L05', 0,  5, "|FIVES      #5C##  |L STRAIGHT #LS## |"),
+        Chrome('L06', 0,  6, "|SIXES      #6C##  |5 OF KIND  #5K## |"),
+        Chrome('L07', 0,  7, "|TOP SCORE  #TS##  |CHANCE     #CH## |"),
+        Chrome('L08', 0,  8, "|BONUS      #TB##  |5K BONUS   #5B## |"),
+        Chrome('L09', 0,  9, "|TOTAL      #UT##  |TOTAL      #LT## |"),
+        Chrome('L09', 0,  9, "├──────────────────┴─────────────────┤"),
+        Chrome('L10', 0, 10, "|         GRAND TOTAL  #GT##         |"),
+        Chrome('L09', 0,  9, "└────────────────────────────────────┘"),
     ]
     return sheet
 
@@ -55,6 +57,9 @@ def get_sheet_for_screen() -> list[Chrome]:
             if char == ' ':
                 inchar = False
             
+            if char == '|':
+                inchar = False
+
             if char == '#':
                 inchar = True
 
